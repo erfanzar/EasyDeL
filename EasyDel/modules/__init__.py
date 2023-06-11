278174d358f94e0e0d03c391a0cd058009ed7f40
erfanzar <erfanzare82@yahoo.com>
2023-06-11 15:33:08 +0330

    adding `GPTJ`

--- EasyDel/modules/__init__.py
+++ EasyDel/modules/__init__.py
@@ -1,3 +1,5 @@
 from .llama import LlamaConfig, LLaMAModel, LLaMAForCausalLM, FlaxLlamaModel, FlaxLlamaForCausalLM
+from .gpt_j import FlaxGPTJModule, FlaxGPTJForCausalLMModule, FlaxGPTJModel, FlaxGPTJForCausalLM, GPTJConfig
 
-__all__ = ['LlamaConfig', 'LLaMAForCausalLM', 'LLaMAModel', 'FlaxLlamaForCausalLM', 'FlaxLlamaModel']
+__all__ = ['LlamaConfig', 'LLaMAForCausalLM', 'LLaMAModel', 'FlaxLlamaForCausalLM', 'FlaxLlamaModel',
+           'FlaxGPTJModule', 'FlaxGPTJForCausalLMModule', 'FlaxGPTJModel', 'FlaxGPTJForCausalLM', 'GPTJConfig']
