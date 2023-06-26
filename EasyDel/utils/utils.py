bf60e31c853c02270639d72310eff8901ebb86d8
erfanzar <erfanzare82@yahoo.com>
2023-06-26 12:32:45 +0330

    testing trainer

--- EasyDel/utils/utils.py
+++ EasyDel/utils/utils.py
@@ -1,7 +1,8 @@
 import jax
 import jax.numpy as jnp
-import numpy as np
-
+import os
+import time
+import wandb
 from jax.experimental.pjit import pjit, with_sharding_constraint as wsc
 from jax.interpreters import pxla
 
@@ -82,3 +83,72 @@ def with_sharding_constraint(x, partition_specs):
     return x
 
 
+class Timer:
+
+    def __init__(self, name):
+        self.name_ = name
+        self.elapsed_ = 0.0
+        self.started_ = False
+        self.start_time = time.time()
+
+    def start(self):
+        assert not self.started_, "timer has already been started"
+        self.start_time = time.time()
+        self.started_ = True
+
+    def stop(self):
+        assert self.started_, "timer is not started"
+        self.elapsed_ += time.time() - self.start_time
+        self.started_ = False
+
+    def reset(self):
+        self.elapsed_ = 0.0
+        self.started_ = False
+
+    def elapsed(self, reset=True):
+        started_ = self.started_
+        if self.started_:
+            self.stop()
+        elapsed_ = self.elapsed_
+        if reset:
+            self.reset()
+        if started_:
+            self.start()
+        return elapsed_
+
+
+class Timers:
+    """Group of timers."""
+
+    def __init__(self, use_wandb, tensorboard_writer):
+        self.timers = {}
+        self.use_wandb = use_wandb
+        self.tensorboard_writer = tensorboard_writer
+
+    def __call__(self, name):
+        if name not in self.timers:
+            self.timers[name] = Timer(name)
+        return self.timers[name]
+
+    def write(self, names, iteration, normalizer=1.0, reset=False):
+
+        assert normalizer > 0.0
+        for name in names:
+            value = self.timers[name].elapsed(reset=reset) / normalizer
+
+            if self.tensorboard_writer:
+                self.tensorboard_writer.add_scalar(f"timers/{name}", value, iteration)
+
+            if self.use_wandb:
+                wandb.log({f"timers/{name}": value}, step=iteration)
+
+    def log(self, names, normalizer=1.0, reset=True, print_rank_0_only=False):
+        assert normalizer > 0.0
+
+        string = "time (ms)"
+        if isinstance(names, str):
+            names = [names]
+        for name in names:
+            elapsed_time = self.timers[name].elapsed(reset=reset) * 1000.0 / normalizer
+            string += " | {} :  {:.2f}".format(name, elapsed_time)
+            print(string, flush=True)
