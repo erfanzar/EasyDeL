7f7596de5d85f16552fe38b5c2b9fed7d253ae87
erfanzar <erfanzare82@yahoo.com>
2023-07-06 11:21:41 +0330

    fixing bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EasyDeL',
-    version='0.0.8',
+    version='0.0.9',
     author='Erfan Zare Chavoshi',
     author_email='erfanzare82@eyahoo.com',
     description='An open-source library to make training faster and more optimized in Jax/Flax',
