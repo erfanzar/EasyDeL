1940e6322c70690232361f0d8e908336f3f01647
erfanzar <erfanzare82@yahoo.com>
2023-07-31 17:11:26 +0330

    version `0.0.18`

--- EasyDel/configs/__init__.py
+++ EasyDel/configs/__init__.py
@@ -1,1 +1,1 @@
-from .configs import llama_configs, falcon_configs, mpt_configs, gptj_configs, opt_configs
+from .configs import llama_configs, falcon_configs, mpt_configs, gptj_configs, opt_configs, llama_2_configs
