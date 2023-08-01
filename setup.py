f84b7f58f70c901be7f105ee12df4bb50efd17c7
erfanzar <erfanzare82@yahoo.com>
2023-08-01 17:05:00 +0330

    version `0.0.19`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EasyDeL',
-    version='0.0.18',
+    version='0.0.19',
     author='Erfan Zare Chavoshi',
     author_email='erfanzare82@eyahoo.com',
     description='An open-source library to make training faster and more optimized in Jax/Flax',
@@ -21,7 +21,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
-    keywords='machine learning, deep learning, pytorch',
+    keywords='machine learning, deep learning, pytorch, jax, flax',
     install_requires=[
         'torch>=1.13.0',
         # add any other required dependencies here
