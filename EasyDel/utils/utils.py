0a209ae64bf4a2e8b4c43027d68bfd2e4e1615e9
erfanzar <erfanzare82@yahoo.com>
2023-08-02 17:13:44 +0330

    TODO : `fix JAXServe bugs`

--- EasyDel/utils/utils.py
+++ EasyDel/utils/utils.py
@@ -156,3 +156,22 @@ class Timers:
             string = prefix_str(f'Time For {name} (ms)', elapsed_time)
 
             print(string, flush=True)
+
+
+class RNG:
+
+    def __init__(self, seed):
+        self.rng = jax.random.PRNGKey(seed)
+
+    def __call__(self, keys=None):
+        if keys is None:
+            self.rng, split_rng = jax.random.split(self.rng)
+            return split_rng
+        elif isinstance(keys, int):
+            split_rngs = jax.random.split(self.rng, num=keys + 1)
+            self.rng = split_rngs[0]
+            return tuple(split_rngs[1:])
+        else:
+            split_rngs = jax.random.split(self.rng, num=len(keys) + 1)
+            self.rng = split_rngs[0]
+            return {key: val for key, val in zip(keys, split_rngs[1:])}
