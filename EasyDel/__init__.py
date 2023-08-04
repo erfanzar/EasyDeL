e98785b631cd087cffd9a8789650b0f687043794
erfanzar <erfanzare82@yahoo.com>
2023-08-04 12:08:18 +0330

    adding training examples

--- EasyDel/__init__.py
+++ EasyDel/__init__.py
@@ -1,19 +1,12 @@
 from .utils.checker import package_checker, is_jax_available, is_torch_available, is_flax_available, \
     is_tensorflow_available
-
-if is_torch_available():
-    ...
-if is_jax_available():
-    from .utils import make_shard_and_gather_fns
-if is_tensorflow_available():
-    ...
-if is_flax_available():
-    from .modules import FlaxLlamaModel, FlaxLlamaForCausalLM, LlamaConfig, \
-        FlaxLTModelModule, FlaxLTConfig, FlaxLTForCausalLM, FlaxLTModel, GPTJConfig, FlaxGPTJModule, \
-        FlaxGPTJForCausalLMModule, FlaxGPTJModel, FlaxGPTJForCausalLM, FlaxMptForCausalLM, MptConfig, FlaxMptModel, \
-        FlaxFalconForCausalLM, FlaxFalconModel, FalconConfig, FlaxGPTNeoXForCausalLM, GPTNeoXConfig, FlaxGPTNeoXModel, \
-        FlaxT5ForConditionalGeneration, FlaxT5Model, FlaxPalmForCausalLM, PalmModel, PalmConfig, T5Config, \
-        FlaxOPTForCausalLM, FlaxOPTModel, OPTConfig
+from .utils import make_shard_and_gather_fns
+from .modules import FlaxLlamaModel, FlaxLlamaForCausalLM, LlamaConfig, \
+    FlaxLTModelModule, FlaxLTConfig, FlaxLTForCausalLM, FlaxLTModel, GPTJConfig, FlaxGPTJModule, \
+    FlaxGPTJForCausalLMModule, FlaxGPTJModel, FlaxGPTJForCausalLM, FlaxMptForCausalLM, MptConfig, FlaxMptModel, \
+    FlaxFalconForCausalLM, FlaxFalconModel, FalconConfig, FlaxGPTNeoXForCausalLM, GPTNeoXConfig, FlaxGPTNeoXModel, \
+    FlaxT5ForConditionalGeneration, FlaxT5Model, FlaxPalmForCausalLM, PalmModel, PalmConfig, T5Config, \
+    FlaxOPTForCausalLM, FlaxOPTModel, OPTConfig
 from .trainer import TrainArguments, fsdp_train_step, get_training_modules, CausalLMTrainer
 
 __all__ = 'package_checker', 'is_jax_available', 'is_torch_available', 'is_flax_available', \
