181f4fcb3f31c13960d8e57ff965322799a2f665
Erfan Zare Chavoshi <59269023+erfanzar@users.noreply.github.com>
2023-09-10 15:36:05 +0330

    Testing `RLHF`

--- EasyDel/rlhf/ppo.py
+++ EasyDel/rlhf/ppo.py
@@ -1,8 +1,10 @@
 import copy
 import math
 
+import einops
 import flax
 import jax
+import transformers
 
 from jax import numpy as jnp
 from jax import lax, random
@@ -75,6 +77,50 @@ class ActorCritic(nn.Module):
         values = self.value_head(critic_embeddings)
 
         return logits, values
+
+    def generate(
+            self,
+            params: Union[flax.core.FrozenDict, dict],
+            input_ids,
+            attention_mask,
+            max_sequence_length: int,
+            eos_token_id: int = None,
+            return_values: bool = False,
+            **kwargs
+    ):
+        b, s = input_ids.shape
+        input_ids = self.model.generate(
+            params=params,
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            generation_config=transformers.GenerationConfig(
+                max_new_tokens=max_sequence_length
+            ),
+
+        ).sequences
+        prompt_mask = einops.repeat(jnp.arange(input_ids.shape[-1]) < s, 's -> b s', b=b)
+        action_mask = ~prompt_mask
+
+        if eos_token_id is not None:
+            attention_mask = (jnp.cumsum(input_ids == eos_token_id, axis=-1) == 0)
+            # mask = F.pad(mask, (1, -1), value=True)  # include eos token
+            action_mask &= attention_mask
+            attention_mask = action_mask & action_mask
+
+        action_logits, value = self(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            return_values=return_values,
+
+        )
+        return PPOActionCriticReturn(
+            input_ids[:, s:],
+            input_ids,
+            attention_mask,
+            prompt_mask,
+            action_logits,
+            value
+        )
 
 
 @functools.partial(jax.jit, static_argnums=0)
