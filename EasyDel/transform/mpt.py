e351928b32562e1112d3ded377b848fa0242c82e
Erfan Zare Chavoshi <59269023+erfanzar@users.noreply.github.com>
2023-09-12 11:43:03 +0330

    Fixing `mpt_from_pretrained` in `transform`

--- EasyDel/transform/mpt.py
+++ EasyDel/transform/mpt.py
@@ -45,22 +45,22 @@ def mpt_convert_pt_to_flax_7b(state_dict, n_layers: int, device=jax.devices('cpu
         state_dict_flax = {('transformer', 'wte', 'embedding'): state_dict[
             'transformer.wte.weight'].cpu().detach().numpy()}
         for i in range(n_layers):
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('norm_1'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'norm_1', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.norm_1.weight'].cpu().detach().numpy()
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('norm_2'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'norm_2', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.norm_2.weight'].cpu().detach().numpy()
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('ffn'), ('down'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'ffn', 'down', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.ffn.down_proj.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('ffn'), ('up'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'ffn', 'up', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.ffn.up_proj.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('w_qkv'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'w_qkv', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.attn.Wqkv.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('wo'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'wo', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.attn.out_proj.weight'].cpu().detach().numpy(), (1, 0))
-        state_dict_flax[('transformer'), ('norm_f'), ('scale')] = state_dict[
+        state_dict_flax[('transformer', 'norm_f', 'scale')] = state_dict[
             f'transformer.norm_f.weight'].cpu().detach().numpy()
         if use_lm_head:
-            state_dict_flax[('lm_head'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('lm_head', 'kernel')] = jnp.transpose(
                 state_dict[f'lm_head.weight'].cpu().detach().numpy(), (1, 0))
     return state_dict_flax
 
@@ -68,29 +68,29 @@ def mpt_convert_pt_to_flax_7b(state_dict, n_layers: int, device=jax.devices('cpu
 def mpt_convert_pt_to_flax_1b(state_dict, n_layers: int, device=jax.devices('cpu')[0], use_lm_head=False, ):
     # CONVERTER MPT-1B
     with jax.default_device(device):
-        state_dict_flax = {(('transformer'), ('wte'), ('embedding')): state_dict[
+        state_dict_flax = {(('transformer', 'wte', 'embedding')): state_dict[
             'transformer.wte.weight'].cpu().detach().numpy()}
         for i in range(n_layers):
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('norm_1'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'norm_1', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.ln_1.weight'].cpu().detach().numpy()
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('norm_2'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'norm_2', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.ln_2.weight'].cpu().detach().numpy()
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('ffn'), ('down'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'ffn', 'down', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.mlp.mlp_down.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('ffn'), ('up'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'ffn', 'up', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.mlp.mlp_up.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('w_qkv'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'w_qkv', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.attn.Wqkv.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('wo'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'wo', 'kernel')] = jnp.transpose(
                 state_dict[f'transformer.blocks.{i}.attn.out_proj.weight'].cpu().detach().numpy(), (1, 0))
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('q_ln'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'q_ln', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.attn.q_ln.weight'].cpu().detach().numpy()
-            state_dict_flax[('transformer'), ('h'), (f'{i}'), ('attn'), ('k_ln'), ('scale')] = state_dict[
+            state_dict_flax[('transformer', 'h', f'{i}', 'attn', 'k_ln', 'scale')] = state_dict[
                 f'transformer.blocks.{i}.attn.k_ln.weight'].cpu().detach().numpy()
-        state_dict_flax[('transformer'), ('norm_f'), ('scale')] = state_dict[
+        state_dict_flax[('transformer', 'norm_f', 'scale')] = state_dict[
             f'transformer.ln_f.weight'].cpu().detach().numpy()
         if use_lm_head:
-            state_dict_flax[('lm_head'), ('kernel')] = jnp.transpose(
+            state_dict_flax[('lm_head', 'kernel')] = jnp.transpose(
                 state_dict[f'lm_head.weight'].cpu().detach().numpy(),
                 (1, 0))
     return state_dict_flax
@@ -147,9 +147,10 @@ def mpt_from_pretrained(model_id, device=jax.devices('cpu')[0], **kwargs):
     """
     config = MptConfig.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True, **kwargs)
+
     easydel_wights = mpt_convert_pt_to_flax_7b(
         state_dict=model.state_dict(),
-        n_layers=config.num_hidden_layers,
+        n_layers=config.num_hidden_layers if hasattr(config, 'num_hidden_layers') else config.n_layers,
         device=device
     )
     config.add_jax_args()
