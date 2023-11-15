3bcc24952d21680d36a70761b28352ed403a108e
erfanzar <erfanzare82@yahoo.com>
2023-11-15 21:36:39 +0330

    remove setting default Device to fix PJRT TPU Device Mismatch Bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EasyDeL',
-    version='0.0.37',
+    version='0.0.38',
     author='Erfan Zare Chavoshi',
     author_email='erfanzare82@eyahoo.com',
     description='An open-source library to make training faster and more optimized in Jax/Flax',
