20ca2e6cb551e44cac90bd0bd4d21f5aa52fa4ed
erfanzar <erfanzare82@yahoo.com>
2023-11-15 20:50:37 +0330

    Version `0.0.37` imports are optimized and python 3.12 is supported

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EasyDeL',
-    version='0.0.36',
+    version='0.0.37',
     author='Erfan Zare Chavoshi',
     author_email='erfanzare82@eyahoo.com',
     description='An open-source library to make training faster and more optimized in Jax/Flax',
@@ -20,6 +20,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     keywords='machine learning, deep learning, pytorch, jax, flax',
     install_requires=[
