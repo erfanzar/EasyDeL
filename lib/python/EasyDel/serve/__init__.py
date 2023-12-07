ed396bb0e9c58cb36635f866a94d5e91a70e96b9
erfanzar <erfanzare82@yahoo.com>
2023-12-07 15:35:10 +0330

    changing JAXServe API

--- lib/python/EasyDel/serve/__init__.py
+++ lib/python/EasyDel/serve/__init__.py
@@ -1,3 +1,3 @@
-from .jax_serve import JAXServer, JaxServerConfig
+from .jax_serve import JAXServer, JAXServerConfig
 from .torch_serve import PyTorchServer, PytorchServerConfig
 from .utils import ChatRequest, InstructRequest
