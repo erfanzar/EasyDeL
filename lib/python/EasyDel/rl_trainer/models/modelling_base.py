46ae978285381f4bee7c45e9eda8f63bd786be2d
erfanzar <erfanzare82@yahoo.com>
2023-12-22 13:39:57 +0330

    Fixing FlaxAutoModelForCausalLMWithValueHead for RL Trainer

--- lib/python/EasyDel/rl_trainer/models/modelling_base.py
+++ lib/python/EasyDel/rl_trainer/models/modelling_base.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from typing import Sequence, Tuple
 import jax
 # from jax import numpy as jnp, lax
 from huggingface_hub import hf_hub_download
@@ -11,6 +12,10 @@ from flax.serialization import from_bytes
 # from flax.traverse_util import flatten_dict, unflatten_dict
 import msgpack
 from safetensors.torch import load_file
+from jax import numpy as jnp
+from ...transform.easydel_transform import huggingface_to_easydel
+from ...modules.auto_models import AutoEasyDelModelForCausalLM
+
 
 LAYER_PATTERNS = [
     "transformer.h.{layer}",
@@ -32,144 +37,63 @@ def match_keywords(string, ts, ns):
 
 class FlaxPreTrainedModelWrapper(nn.Module):
     pretrained_model: FlaxPreTrainedModel
-    transformers_parent_class = None
+    transformers_parent_class = AutoEasyDelModelForCausalLM
     supported_args = None
     supported_modules = ("v_head",)
     supported_rm_modules = ("score",)
     supported_pretrained_model_architectures = FlaxPreTrainedModel
 
     @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, from_pt: bool = True, *model_args, **kwargs):
-
-        """
-        The from_pretrained function is used to instantiate a model from a pretrained checkpoint.
-
-        :param cls: Refer to the class that called this function
-        :param pretrained_model_name_or_path: Specify the path to the pretrained model
-        :param from_pt: bool: Determine whether to load the model from a pytorch checkpoint or not
-        :param model_args: Pass the positional arguments of the model
-        :param kwargs: Pass keyworded, variable-length argument list
-        :return: A model with the state_dict loaded from a file
-        
-        """
-        if kwargs is not None:
-            reward_adapter = kwargs.pop("reward_adapter", None)
-            trl_model_args, pretrained_kwargs, peft_quantization_kwargs = cls._split_kwargs(kwargs)
-            token = pretrained_kwargs.get("token", None)
-        else:
-            reward_adapter = None
-            trl_model_args = {}
-            pretrained_kwargs = {}
-            token = None
-
-        if reward_adapter is not None and not isinstance(reward_adapter, str):
-            raise ValueError(
-                "The `reward_adapter` argument should be "
-                "a string representing the name of local path or the"
-                " Hub id to the Reward Modeling adapter."
-            )
-
-        if isinstance(pretrained_model_name_or_path, str):
-            pretrained_model = cls.transformers_parent_class.from_pretrained(
-                pretrained_model_name_or_path, *model_args, **pretrained_kwargs
-            )
-
-        elif isinstance(pretrained_model_name_or_path, cls.supported_pretrained_model_architectures):
-            pretrained_model = pretrained_model_name_or_path
-
-        else:
-            raise ValueError(
-                "pretrained_model_name_or_path should be a string or a PreTrainedModel, "
-                f"but is {type(pretrained_model_name_or_path)}"
-            )
-
-        model = cls(pretrained_model, **trl_model_args)
-        is_resuming_training = True
-        if isinstance(pretrained_model_name_or_path, str):
-            safe_filename = os.path.join(pretrained_model_name_or_path, "model.safetensors")
-            filename = os.path.join(pretrained_model_name_or_path, "pytorch_model.bin")
-
-            sharded_index_filename = os.path.join(pretrained_model_name_or_path, "pytorch_model.bin.index.json")
-            safe_sharded_index_filename = os.path.join(pretrained_model_name_or_path, "model.safetensors.index.json")
-            is_sharded = False
-            use_safe = os.path.exists(safe_filename)
-
-            if not (os.path.exists(filename) or os.path.exists(safe_filename)):
-                filename, files_to_download, is_sharded, is_resuming_training = cls._get_checkpoint_from_hub(
-                    pretrained_model,
-                    pretrained_model_name_or_path,
-                    sharded_index_filename,
-                    token=token,
-                )
-                if filename is None and files_to_download is None:
-                    safe_filename, files_to_download, is_sharded, is_resuming_training = cls._get_checkpoint_from_hub(
-                        pretrained_model,
-                        pretrained_model_name_or_path,
-                        safe_sharded_index_filename,
-                        token=token,
-                        model_name="model.safetensors",
-                        model_index_name="model.safetensors.index.json",
-                    )
-                    use_safe = True
-                else:
-                    use_safe = False
-            if from_pt:
-                loading_func = load_file
-                load_kwargs = {}
-            else:
-                def loading_func(file_name: str, *args, **kwargs_):
-                    tensors = {}
-                    with open(file_name, 'rb') as stream:
-                        unpacker = msgpack.Unpacker(stream, read_size=83886080, max_buffer_size=0)
-                        for key, value in unpacker:
-                            key = tuple(key)
-                            tensor = from_bytes(None, value)
-                            tensors[key] = tensor
-                    return tensors
-
-            if is_resuming_training:
-                if is_sharded:
-                    state_dict = {}
-
-                    for shard_file in files_to_download:
-                        filename = hf_hub_download(
-                            pretrained_model_name_or_path,
-                            shard_file,
-                            token=token,
-                        )
-                        state_dict.update(loading_func(filename, **load_kwargs))
-                else:
-                    state_dict = loading_func(filename if not use_safe else safe_filename, **load_kwargs)
-
-        else:
-            state_dict = pretrained_model_name_or_path.state_dict()
-
-        if from_pt:
-            lw = len('.weight')
-            with jax.default_device(cls._get_current_device()):
-                flax_dict = {}
-                for key, tensor in state_dict.items():
-                    if match_keywords(key, ['kernel'], ['none']):
-                        if len(tensor.shape) == 2:
-                            tensor = tensor.transpose(0, 1)
-                    if key.endswith('.weight'):
-                        key = key[:-lw] + '.kernel'
-                    key_tuple = key.split('.')
-                    key_names = ()
-                    tensor = tensor.detach().cpu().numpy()
-                    for k in key_tuple:
-                        key_names += k,
-                    flax_dict[key_names] = tensor
-
-        model.is_peft_model = False
-        model.current_device = cls._get_current_device()
-
-        if is_resuming_training:
-            model.post_init(state_dict=state_dict)
-
-        model.supports_rm_adapter = False
-
-        return model
+    def from_pretrained(
+        cls,
+        pretrained_model_name_or_path,
+        summary_dropout_prob: float = 0.0,
+        device=jax.devices('cpu')[0],
+        dtype: jax.numpy.dtype = jax.numpy.float32,
+        param_dtype: jax.numpy.dtype = jax.numpy.float32,
+        precision: jax.lax.Precision = jax.lax.Precision('fastest'),
+        sharding_axis_dims: Sequence[int] = (1, -1, 1, 1),
+        sharding_axis_names: Sequence[str] = ("dp", "fsdp", "tp", "sp"),
+        q_ps: jax.sharding.PartitionSpec = jax.sharding.PartitionSpec(
+            ("dp", "fsdp"), "sp", "tp", None),
+        k_ps: jax.sharding.PartitionSpec = jax.sharding.PartitionSpec(
+            ("dp", "fsdp"), "sp", "tp", None),
+        v_ps: jax.sharding.PartitionSpec = jax.sharding.PartitionSpec(
+            ("dp", "fsdp"), "sp", "tp", None),
+        b_ps: jax.sharding.PartitionSpec = jax.sharding.PartitionSpec(
+            ("dp", "fsdp"), None, None, None),
+        a_ps: jax.sharding.PartitionSpec = jax.sharding.PartitionSpec(
+            ("dp", "fsdp"), "sp", "tp", None),
+        use_shard_map: bool = False,
+        input_shape: Sequence[int] = (1, 1),
+    ) -> Tuple[FlaxPreTrainedModel, dict]:
+        model, params = cls.transformers_parent_class.from_pretrained(
+            pretrained_model_name_or_path,
+            dtype=dtype,
+            param_dtype=param_dtype,
+            device=device,
+            precision=precision,
+            sharding_axis_dims=sharding_axis_dims,
+            sharding_axis_names=sharding_axis_names,
+            q_ps=q_ps,
+            k_ps=k_ps,
+            v_ps=v_ps,
+            b_ps=b_ps,
+            a_ps=a_ps,
+            use_shard_map=use_shard_map,
+            input_shape=input_shape
+        )
+        config = model.config
+        model_type = getattr(config, "model_type", "UNKNOWN")
+        rl_model = cls(
+            pretrained_model=model,
+            transformers_parent_class=cls.transformers_parent_class,
+            summary_dropout_prob=summary_dropout_prob,
+            dtype=dtype,
+            param_dtype=param_dtype,
+            precision=precision,
+        )
+        return rl_model, params
 
     @classmethod
     def _get_checkpoint_from_hub(
@@ -195,7 +119,7 @@ class FlaxPreTrainedModelWrapper(nn.Module):
         :param model_index_name: Specify the name of the index file
         :param : Load the pretrained model
         :return: A tuple of four elements:
-        
+
         """
         files_to_download = None
         filename = None
@@ -246,7 +170,7 @@ class FlaxPreTrainedModelWrapper(nn.Module):
 
         :param cls: Indicate that the function is a method of the class
         :return: The current device
-        
+
         """
         return jax.devices()[0]
 
@@ -261,7 +185,7 @@ class FlaxPreTrainedModelWrapper(nn.Module):
         :param cls: Refer to the class itself
         :param kwargs: Pass keyword arguments to the function
         :return: A tuple of three dictionaries
-        
+
         """
         supported_kwargs = {}
         unsupported_kwargs = {}
@@ -301,7 +225,6 @@ class FlaxPreTrainedModelWrapper(nn.Module):
         raise NotImplementedError
 
     def compute_reward_score(self, input_ids, attention_mask=None, ppo_adapter_name="default", **kwargs):
-
         """
         The compute_reward_score function is used to compute the reward score for a given input.
         The function takes in an input_ids tensor and returns a tensor of scores. The shape of the returned
@@ -313,10 +236,11 @@ class FlaxPreTrainedModelWrapper(nn.Module):
         :param ppo_adapter_name: Set the adapter back to its original state
         :param **kwargs: Pass a variable number of arguments to a function
         :return: The scores for the given input_ids
-        
+
         """
         if not self.supports_rm_adapter:
-            raise ValueError("This model does not support reward modeling adapter.")
+            raise ValueError(
+                "This model does not support reward modeling adapter.")
 
         # enable rm adapter
         self.pretrained_model.set_adapter(self.rm_adapter_name)
