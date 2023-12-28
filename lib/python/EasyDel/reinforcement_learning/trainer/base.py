b5cf6b1441f9f402c3ce5f02748739af98038a25
erfanzar <erfanzare82@yahoo.com>
2023-12-28 18:41:45 +0330

    Addin PPO base trainer

--- lib/python/EasyDel/reinforcement_learning/trainer/base.py
+++ lib/python/EasyDel/reinforcement_learning/trainer/base.py
@@ -1,32 +1,48 @@
-from huggingface_hub import ModelHubMixin
+from fjformer import with_sharding_constraint
+from fjformer.func.loss_func import cross_entropy_loss_and_accuracy
+from jax import numpy as jnp
+import jax
+from jax.sharding import PartitionSpec
 
 
-class BaseTrainer(ModelHubMixin):
-    r"""
-    Base class for all trainers - this base class implements the basic functions that we
-    need for a trainer.
+def create_reinforcement_learning_casual_language_model_train_step(partition_spec=PartitionSpec(("dp", "fsdp"), "sp")):
+    """
+    The create_reinforcement_learning_casual_language_model_train_step function is a training step function
+    that takes in the current state of the model, and a batch of data. It then calculates the loss and accuracy for
+    this batch, and returns an updated state with new parameters based on these gradients.
 
-    The trainer needs to have the following functions:
-        - step: takes in a batch of data and performs a step of training
-        - loss: takes in a batch of data and returns the loss
-        - compute_rewards: takes in a batch of data and returns the rewards
-        - _build_models_and_tokenizer: builds the models and tokenizer
-        - _build_dataset: builds the dataset
-    Each user is expected to implement their own trainer class that inherits from this base
-    if they want to use a new training algorithm.
+    :param partition_spec: Specify which devices the model will be split across
+    :return: A reinforcement_learning_casual_language_model_train_step function that takes in the current state
+    of the model,
+
     """
 
-    def __init__(self, config):
-        self.config = config
+    def reinforcement_learning_casual_language_model_train_step(state, batch):
+        """
+        The casual_language_model_train_step function is a training step function that takes in the current state
+        of the model, and a batch of data. It then calculates the loss and accuracy for this batch,
+        and returns an updated state with new parameters based on these gradients.
 
-    def step(self, *args):
-        raise NotImplementedError("Not implemented")
+        :param state: Store the model parameters
+        :param batch: Pass the data to the model
+        :return: A tuple of (state, loss, accuracy)
 
-    def loss(self, *args):
-        raise NotImplementedError("Not implemented")
+        """
+        batch = with_sharding_constraint(batch, partition_spec)
 
-    def compute_rewards(self, *args):
-        raise NotImplementedError("Not implemented")
+        def calculate_loss(params):
+            labels = batch.pop("labels")
+            logits = state.apply_fn(params=params, **batch,
+                                    return_dict=True).logits
 
-    def _save_pretrained(self, save_directory):
-        raise NotImplementedError("Not implemented")
+            loss, accuracy = cross_entropy_loss_and_accuracy(
+                logits[:, :-1, :], labels, batch["attention_mask"].astype(jnp.float32)[:, 1:]
+            )
+            return loss, accuracy
+
+        grad_fn = jax.value_and_grad(calculate_loss, has_aux=True)
+        (loss__, accuracy__), grad = grad_fn(state.params)
+        state = state.apply_gradients(grads=grad)
+        return state, loss__, accuracy__
+
+    return reinforcement_learning_casual_language_model_train_step
--- lib/python/EasyDel/reinforcement_learning/trainer/ppo_trainer.py
+++ lib/python/EasyDel/reinforcement_learning/trainer/ppo_trainer.py
@@ -1,1 +1,46 @@
+import chex
+import jax.scipy.special
 from jax import grad, jit, numpy as jnp, lax
+
+
+class PPOTrainer:
+    def __init__(self):
+        ...
+
+    def step(self):
+        ...
+
+    def generate(self):
+        ...
+
+    def prepare_model_inputs(self, queries: chex.Array, responses: chex.Array):
+        ...
+
+    def compute_rewards(self):
+        ...
+
+    @staticmethod
+    def _kl_penalty(kl_penalty: str, logprobs: chex.Array, ref_logprobs: chex.Array) -> chex.Array:
+        if kl_penalty == "kl":
+            return logprobs - ref_logprobs
+
+        if kl_penalty == "abs":
+            return (logprobs - ref_logprobs).abs()
+
+        if kl_penalty == "mse":
+            return 0.5 * (logprobs - ref_logprobs).square()
+
+        if kl_penalty == "full":
+            return jnp.sum(
+                jax.scipy.special.kl_div(
+                    ref_logprobs,
+                    logprobs,
+                ),
+                axis=-1
+            )
+
+    def compute_advantages(self, values: chex.Array, rewards: chex.Array, mask: chex.Array):
+        ...
+
+    def loss(self):
+        ...
