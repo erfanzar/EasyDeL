de2ec4e7c1f13f091683c94953fc7871d52bfa55
erfanzar <erfanzare82@yahoo.com>
2024-01-11 04:39:41 -0800

    Version `0.42.0` New Features `EasyDelState`,`fully automated loading and converting model`

--- lib/python/EasyDel/__init__.py
+++ lib/python/EasyDel/__init__.py
@@ -114,39 +114,27 @@ from .smi import (
     get_mem as get_mem
 )
 
-from .transform.llama import (
-    llama_from_pretrained as llama_from_pretrained,
-    llama_convert_flax_to_pt as llama_convert_flax_to_pt,
-    llama_convert_hf_to_flax_load as llama_convert_hf_to_flax_load,
-    llama_convert_hf_to_flax as llama_convert_hf_to_flax,
-    llama_easydel_to_hf as llama_easydel_to_hf
-)
-from .transform.mpt import (
-    mpt_convert_flax_to_pt_1b as mpt_convert_flax_to_pt_1b,
-    mpt_convert_pt_to_flax_1b as mpt_convert_pt_to_flax_1b,
-    mpt_convert_pt_to_flax_7b as mpt_convert_pt_to_flax_7b,
-    mpt_convert_flax_to_pt_7b as mpt_convert_flax_to_pt_7b,
-    mpt_from_pretrained as mpt_from_pretrained
-)
-
-from .transform.falcon import (
-    falcon_convert_pt_to_flax_7b as falcon_convert_pt_to_flax_7b,
+from .transform import (
+    huggingface_to_easydel as huggingface_to_easydel,
+    easystate_to_huggingface_model as easystate_to_huggingface_model,
+    easystate_to_torch as easystate_to_torch,
     falcon_convert_flax_to_pt_7b as falcon_convert_flax_to_pt_7b,
     falcon_from_pretrained as falcon_from_pretrained,
     falcon_convert_hf_to_flax as falcon_convert_hf_to_flax,
-    falcon_easydel_to_hf as falcon_easydel_to_hf
-)
-from .transform.mistral import (
-    mistral_convert_hf_to_flax as mistral_convert_hf_to_flax,
+    mpt_convert_pt_to_flax_1b as mpt_convert_pt_to_flax_1b,
+    mpt_convert_pt_to_flax_7b as mpt_convert_pt_to_flax_7b,
+    mpt_convert_flax_to_pt_7b as mpt_convert_flax_to_pt_7b,
+    mpt_from_pretrained as mpt_from_pretrained,
     mistral_convert_hf_to_flax_load as mistral_convert_hf_to_flax_load,
     mistral_convert_flax_to_pt as mistral_convert_flax_to_pt,
     mistral_from_pretrained as mistral_from_pretrained,
-    mistral_convert_pt_to_flax as mistral_convert_pt_to_flax,
-    mistral_easydel_to_hf as mistral_easydel_to_hf
-)
-
-from .transform.easydel_transform import (
-    huggingface_to_easydel as huggingface_to_easydel
+    falcon_convert_pt_to_flax_7b as falcon_convert_pt_to_flax_7b,
+    mistral_convert_hf_to_flax as mistral_convert_hf_to_flax,
+    mpt_convert_flax_to_pt_1b as mpt_convert_flax_to_pt_1b,
+    llama_convert_flax_to_pt as llama_convert_flax_to_pt,
+    llama_convert_hf_to_flax_load as llama_convert_hf_to_flax_load,
+    llama_convert_hf_to_flax as llama_convert_hf_to_flax,
+    llama_from_pretrained as llama_from_pretrained
 )
 from .etils import (
     EasyDelOptimizers as EasyDelOptimizers,
--- lib/python/EasyDel/transform/__init__.py
+++ lib/python/EasyDel/transform/__init__.py
@@ -1,10 +1,31 @@
-from .llama import llama_from_pretrained, llama_convert_flax_to_pt, llama_convert_hf_to_flax_load, \
-    llama_convert_hf_to_flax, llama_easydel_to_hf
-from .mpt import mpt_convert_flax_to_pt_1b, mpt_convert_pt_to_flax_1b, mpt_convert_pt_to_flax_7b, \
-    mpt_convert_flax_to_pt_7b, mpt_from_pretrained
-from .falcon import falcon_convert_pt_to_flax_7b, falcon_convert_flax_to_pt_7b, falcon_from_pretrained, \
-    falcon_convert_hf_to_flax, falcon_easydel_to_hf
-from .mistral import mistral_convert_hf_to_flax, mistral_convert_hf_to_flax_load, \
-    mistral_convert_flax_to_pt, \
-    mistral_from_pretrained, mistral_convert_pt_to_flax, mistral_easydel_to_hf
-from .easydel_transform import easystate_to_huggingface_model, easystate_to_torch
+from .llama import (
+    llama_from_pretrained,
+    llama_convert_flax_to_pt,
+    llama_convert_hf_to_flax_load,
+    llama_convert_hf_to_flax,
+)
+from .mpt import (
+    mpt_convert_flax_to_pt_1b,
+    mpt_convert_pt_to_flax_1b,
+    mpt_convert_pt_to_flax_7b,
+    mpt_convert_flax_to_pt_7b,
+    mpt_from_pretrained
+)
+from .falcon import (
+    falcon_convert_pt_to_flax_7b,
+    falcon_convert_flax_to_pt_7b,
+    falcon_from_pretrained,
+    falcon_convert_hf_to_flax,
+)
+from .mistral import (
+    mistral_convert_hf_to_flax,
+    mistral_convert_hf_to_flax_load,
+    mistral_convert_flax_to_pt,
+    mistral_from_pretrained,
+)
+
+from .easydel_transform import (
+    huggingface_to_easydel,
+    easystate_to_huggingface_model,
+    easystate_to_torch
+)
