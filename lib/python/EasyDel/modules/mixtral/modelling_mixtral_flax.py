48a7a15a8b0722f5d4fdbb35dd737a0379a1b775
erfanzar <erfanzare82@yahoo.com>
2024-01-22 20:46:12 +0330

    Adding `AttentionCache` to Mixtral Models

--- lib/python/EasyDel/modules/mixtral/modelling_mixtral_flax.py
+++ lib/python/EasyDel/modules/mixtral/modelling_mixtral_flax.py
@@ -161,7 +161,7 @@ class FlaxMixtralAttention(nn.Module):
             query_partition_spec=self.config.query_partition_spec,
             value_partition_spec=self.config.value_partition_spec,
             mesh=self.config.jax_mesh(),
-            sm_scale=1 # TOBE CHANGED
+            sm_scale=1  # TOBE CHANGED
         )
 
     @nn.compact
@@ -211,6 +211,50 @@ class FlaxMixtralAttention(nn.Module):
 
     def _merge_heads(self, hidden_states):
         return hidden_states.reshape(hidden_states.shape[:2] + (self.hidden_size,))
+
+    @nn.compact
+    def _concatenate_to_cache(self, key, value, query, attention_mask):
+        """
+        The _concatenate_to_cache function is used to concatenate the key and value vectors
+        of a query with those of previous queries. This allows for the attention mechanism to
+        look at all previous queries when computing its output. The function takes in three
+        arguments: key, value, and query. It also uses two variables that are stored in the cache:
+        cached_key and cached_value.
+
+        :param self: Access the variables stored in the cache
+        :param key: Store the keys of the encoder-decoder attention
+        :param value: Initialize the cached_value variable
+        :param query: Determine the number of cache vectors to update
+        :param attention_mask: Mask out the padded vectors in the cache
+        :return: The key, value and attention_mask
+
+        """
+        is_initialized = self.has_variable("cache", "cached_key")
+        cached_key = self.variable(
+            "cache", "cached_key", jnp.zeros, key.shape, key.dtype)
+        cached_value = self.variable(
+            "cache", "cached_value", jnp.zeros, value.shape, value.dtype)
+        cache_index = self.variable(
+            "cache", "cache_index", lambda: jnp.array(0, dtype=jnp.int32))
+
+        if is_initialized:
+            *batch_dims, max_length, num_heads, depth_per_head = cached_key.value.shape
+            cur_index = cache_index.value
+            indices = (0,) * len(batch_dims) + (cur_index, 0, 0)
+            key = lax.dynamic_update_slice(cached_key.value, key, indices)
+            value = lax.dynamic_update_slice(
+                cached_value.value, value, indices)
+            cached_key.value = key
+            cached_value.value = value
+            num_updated_cache_vectors = query.shape[1]
+            cache_index.value = cache_index.value + num_updated_cache_vectors
+
+            pad_mask = jnp.broadcast_to(
+                jnp.arange(max_length) < cur_index + num_updated_cache_vectors,
+                tuple(batch_dims) + (1, num_updated_cache_vectors, max_length),
+            )
+            attention_mask = combine_masks(pad_mask, attention_mask)
+        return key, value, attention_mask
 
     def __call__(
             self,
