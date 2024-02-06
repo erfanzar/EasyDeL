62f875783cdd61503f082de036e4dab347c15bae
erfanzar <erfanzare82@yahoo.com>
2024-02-05 16:18:56 -0800

    adding modules `FlaxQwen1ForCausalLM`, `FlaxQwen1Model`, `FlaxQwen1ForSequenceClassification` _all tested!_

--- lib/python/EasyDel/modules/qwen1/__init__.py
+++ lib/python/EasyDel/modules/qwen1/__init__.py
@@ -1,6 +1,6 @@
 from .qwen1_configuration import Qwen1Config
 from .modelling_qwen1_flax import (
-    FlaxQwen1ForCausalLM,
-    FlaxQwen1Model,
-    FlaxQwen1ForSequenceClassification
+    FlaxQwen1ForCausalLM as FlaxQwen1ForCausalLM,
+    FlaxQwen1Model as FlaxQwen1Model,
+    FlaxQwen1ForSequenceClassification as FlaxQwen1ForSequenceClassification
 )
