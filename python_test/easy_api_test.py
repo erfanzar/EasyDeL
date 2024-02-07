eede4c1dc075195eb6a1601c4979bb9b4e0335e9
erfanzar <erfanzare82@yahoo.com>
2024-02-07 15:27:37 +0330

    `Mixtral` models are pjit compatible but they are slower than usual (still faster than pytorch version)

--- python_test/easy_api_test.py
+++ python_test/easy_api_test.py
@@ -2,29 +2,22 @@ import flax.core
 import transformers
 
 from lib.python.EasyDel.serve.api.serve import EasyServe, EasyServeConfig
-from lib.python.EasyDel import Qwen1Config, FlaxQwen1ForCausalLM
+from lib.python.EasyDel import MixtralConfig, FlaxMixtralForCausalLM
 from jax import numpy as jnp, lax
-
-
-def modify_qwen_tokenizer(tokenizer):
-    tokenizer.eos_token_id = tokenizer.im_end_id
-    tokenizer.bos_token_id = tokenizer.im_start_id
-    tokenizer.pad_token_id = tokenizer.im_end_id
-    return tokenizer
 
 
 def main():
     max_position_embeddings = 512
-    config = Qwen1Config(
+    config = MixtralConfig(
         hidden_size=256,
         intermediate_size=200,
         max_position_embeddings=max_position_embeddings,
         num_hidden_layers=4,
         num_attention_heads=2,
-        cv_channels=64
+        num_key_value_heads=2
     )
 
-    model = FlaxQwen1ForCausalLM(
+    model = FlaxMixtralForCausalLM(
         config=config,
         dtype=jnp.float16,
         param_dtype=jnp.float16,
@@ -45,21 +38,21 @@ def main():
         use_prefix_tokenizer=False
     )
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained("Qwen/Qwen-1_8B-Chat", trust_remote_code=True)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        trust_remote_code=True
+    )
     server = EasyServe.from_parameters(
         llm=model,
         params={
             "params": params,
         },
         serve_config=serve_config,
-        tokenizer=modify_qwen_tokenizer(tokenizer),
+        tokenizer=tokenizer,
         partition_rules=config.get_partition_rules(True),
         shard_parameters=False,
     )
-    server.tokenizer = modify_qwen_tokenizer(server.tokenizer)
     print(server)
-
-    server.prefix_tokenizer = modify_qwen_tokenizer(server.prefix_tokenizer)
     server.fire()
 
 
