c36120bda4d9f0ebe87985caaeb66088a0821827
erfanzar <erfanzare82@yahoo.com>
2024-02-22 13:36:05 +0330

    Updating Examples

--- lib/python/EasyDel/serve/prompters/__init__.py
+++ lib/python/EasyDel/serve/prompters/__init__.py
@@ -3,3 +3,4 @@ from .guanaco_prompter import GuanacoPrompter
 from .llama2_prompter import Llama2Prompter
 from .openchat_prompter import OpenChatPrompter
 from .qwen2_prompter import Qwen2Prompter
+from .gemma_prompter import GemmaPrompter
