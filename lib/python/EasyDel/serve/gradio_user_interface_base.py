9d99b82232c1480d2f186e5cb7e9b3fc6c7a3805
erfanzar <erfanzare82@yahoo.com>
2024-02-27 13:18:45 +0330

    Editing PyTorchServer and JAXServer

--- lib/python/EasyDel/serve/gradio_user_interface_base.py
+++ lib/python/EasyDel/serve/gradio_user_interface_base.py
@@ -188,3 +188,37 @@ class GradioUserInference:
                 max_compile_tokens=max_compile_tokens
             )
         return block
+
+    def __repr__(self):
+
+        """
+        The __repr__ function is used to generate a string representation of an object.
+        This function should return a string that can be parsed by the Python interpreter
+        to recreate the object. The __repr__ function is called when you use print() on an
+        object, or when you type its name in the REPL.
+
+        :param self: Refer to the instance of the class
+        :return: A string representation of the object
+        """
+        string = f"{self.__class__.__name__}(\n"
+        for k, v in self.__dict__.items():
+            if not k.startswith("_"):
+
+                try:
+                    repr_src = f"\t{k} : " + v.__str__().replace("\n", "\n\t") + "\n"
+                    string += repr_src if len(repr_src) < 500 else f"\t{k} : " + f"{v.__class__.__name__}(...)" + "\n"
+                except TypeError:
+                    ...
+
+        return string + ")"
+
+    def __str__(self):
+
+        """
+        The __str__ function is called when you use the print function or when str() is used.
+        It should return a string representation of the object.
+
+        :param self: Refer to the instance of the class
+        :return: The object's string representation
+        """
+        return self.__repr__()
