44cdaf61fc4d9402b5bf0667c8ec823374a56b7a
erfanzar <erfanzare82@yahoo.com>
2024-02-29 13:26:41 +0330

    Working on StableLM models

--- lib/python/EasyDel/serve/prompters/__init__.py
+++ lib/python/EasyDel/serve/prompters/__init__.py
@@ -2,5 +2,6 @@ from .cargo_prompter import CargoPrompter
 from .guanaco_prompter import GuanacoPrompter
 from .llama2_prompter import Llama2Prompter
 from .openchat_prompter import OpenChatPrompter
-from .qwen2_prompter import Qwen2Prompter
+from .chatml_prompter import ChatMLPrompter
 from .gemma_prompter import GemmaPrompter
+from .zephyr_prompter import ZephyrPrompter
