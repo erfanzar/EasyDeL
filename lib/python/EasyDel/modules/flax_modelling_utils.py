bd57fd464e9642a366fa6c8ab00cd0956d8ae4b3
erfanzar <erfanzare82@yahoo.com>
2024-03-13 13:58:52 +0330

    Improving Gemma

--- lib/python/EasyDel/modules/flax_modelling_utils.py
+++ lib/python/EasyDel/modules/flax_modelling_utils.py
@@ -19,7 +19,7 @@ from jax.sharding import PartitionSpec
 from ..etils.errors import EasyDelBlockWiseFFNError
 
 ACT2FN = {
-    "gelu": partial(nn.gelu, approximate=True),
+    "gelu": partial(nn.gelu, approximate=False),
     "relu": nn.relu,
     "silu": nn.swish,
     "swish": nn.swish,
