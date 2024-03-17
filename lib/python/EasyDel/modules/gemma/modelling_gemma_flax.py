61d8d16b0cef215ece7c85805b12738d9bdc8a41
erfanzar <erfanzare82@yahoo.com>
2024-03-17 14:17:22 +0330

    Improving sharding for LLMs with `size over 80B` and context length higher than `2M` tokens

--- lib/python/EasyDel/modules/gemma/modelling_gemma_flax.py
+++ lib/python/EasyDel/modules/gemma/modelling_gemma_flax.py
@@ -11,7 +11,7 @@ import numpy as np
 from fjformer import with_sharding_constraint
 from flax.core.frozen_dict import FrozenDict, freeze, unfreeze
 from flax.linen import combine_masks, make_causal_mask
-from flax.linen.attention import dot_product_attention_weights
+
 from flax.traverse_util import flatten_dict, unflatten_dict
 from jax import lax
 from transformers.modeling_flax_outputs import FlaxBaseModelOutput, FlaxCausalLMOutput
