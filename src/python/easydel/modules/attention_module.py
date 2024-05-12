7ad264a81c1f34d727f24eeb8f36ef9b6f917758
erfanzar <erfanzare82@yahoo.com>
2024-05-12 14:07:42 +0330

    improving attention module

--- src/python/easydel/modules/attention_module.py
+++ src/python/easydel/modules/attention_module.py
@@ -1169,6 +1169,8 @@ class AttentionModule:
                 output_diff = diff(excepted_output, out)
                 g_diff = [diff(*args) for args in zip(excepted_grads, grad)]
                 sum_g = sum(g_diff)
+                sum_g = jax.device_get(sum_g)
+                output_diff = jax.device_get(output_diff)
                 frame_out[key.upper()] = {
                     "OUT DIFF": output_diff,
                     "GRADIENT DIFF SUM": sum_g,
