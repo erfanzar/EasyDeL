50e2bb90b280a4660ee2db592800cb6ccc76f0a9
erfanzar <erfanzare82@yahoo.com>
2024-06-02 20:51:02 +0330

    Releasing `0.0.67` and making imports 91x times faster

--- python_test/import_test.py
+++ python_test/import_test.py
@@ -4,6 +4,7 @@ import time
 def main():
     start = time.time()
     import src.python.easydel as ed
+    config = ed.LlamaConfig()
     end = time.time()
     print(f"time took for import {end - start}")
 
