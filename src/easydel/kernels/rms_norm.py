b513de89e9aea8ce7199dd1fb2a4f19ca582d4c6
erfanzar <erfanzare82@yahoo.com>
2024-06-16 15:42:15 +0330

    fixing some fjformer errors

--- src/easydel/kernels/rms_norm.py
+++ src/easydel/kernels/rms_norm.py
@@ -13,17 +13,17 @@ def _rms_norm_forward_kernel(
         w_ref,
         x_ref,
         o_ref,
+        r_ref,
         *,
         eps,
         dim,
-        block_size  # UNUSED
 ):
-    idx = pl.program_id(0)  # UNUSED
     org_dtype = x_ref[...].dtype
     X_R = x_ref[...].astype(jnp.float32)  # make sure it's always in Float32
-    o_ref[...] = (
-            w_ref[...] * (X_R * jax.lax.rsqrt(((jnp.sum(X_R * X_R, -1, keepdims=True) / dim) + eps)))
-    ).astype(org_dtype)
+    inv_var = jax.lax.rsqrt((jnp.sum(X_R * X_R, -1, keepdims=True) / dim) + eps)
+    r_ref[...] = inv_var
+    normed = (X_R * inv_var).astype(w_ref.dtype)
+    o_ref[...] = (w_ref[...] * normed).astype(org_dtype)
 
 
 def _rms_norm_forward_main(
@@ -36,31 +36,40 @@ def _rms_norm_forward_main(
     W = W.reshape(1, -1)
     X = X.reshape(-1, DIM)
     block_size, num_wraps = calculate_settings(DIM, X.dtype)
+
+    out_shape = [
+        jax.ShapeDtypeStruct(shape=X.shape, dtype=X.dtype, sharding=X.sharding),
+        jax.ShapeDtypeStruct(shape=(S, 1), dtype=jnp.float32)
+    ]
+    in_specs = [
+        pl.BlockSpec(lambda i: (0, 0), (1, DIM,)),
+        pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
+    ]
+    out_specs = [
+        pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
+        pl.BlockSpec(lambda i: (0, 0), (S, 1)),
+    ]
     method = pl.pallas_call(
         functools.partial(
             _rms_norm_forward_kernel,
             eps=eps,
             dim=DIM,
-            block_size=block_size,
         ),
-        out_shape=jax.ShapeDtypeStruct(shape=X.shape, dtype=X.dtype, sharding=X.sharding),
-        in_specs=[
-            pl.BlockSpec(lambda i: (0, 0), (1, DIM,)),
-            pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
-        ],
-        out_specs=pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
+        out_shape=out_shape,
+        in_specs=in_specs,
+        out_specs=out_specs,  # type:ignore
         grid=(block_size,),
         interpret=interpret,
         name="rms_forward_main",
         debug=True
     )
-    result = method(W, X)
-    return result.reshape(B, S, DIM).astype(X.dtype)
+    result, R = method(W, X)
+    return result.reshape(B, S, DIM)
 
 
 if __name__ == "__main__":
     inputs = jax.random.normal(jax.random.key(564), (1, 256, 64), dtype=jnp.float16)
-
+    print(inputs.devices())
     norm = RMSNorm(
         64,
         1e-6,
@@ -70,6 +79,6 @@ if __name__ == "__main__":
     params = norm.init(jax.random.PRNGKey(0), inputs)
 
     out = norm.apply(params, inputs)
+
     norm_kernel_out = _rms_norm_forward_main(params["params"]["kernel"], inputs, 1e-6, interpret=True)
-    print(out)
-    print(norm_kernel_out)
+    print(jnp.allclose(out, norm_kernel_out))
