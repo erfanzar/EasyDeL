dd0f4afc299a59b4d9822a8568d0acdefb42aa8c
erfanzar <erfanzare82@yahoo.com>
2024-07-28 03:04:08 -0700

    removing dec info

--- src/easydel/trainers/vision_causal_language_model_trainer/vision_causal_language_model_trainer.py
+++ src/easydel/trainers/vision_causal_language_model_trainer/vision_causal_language_model_trainer.py
@@ -299,7 +299,6 @@ class VisionCausalLanguageModelTrainer(CausalLanguageModelTrainer):
                     if self.arguments.remove_ckpt_after_load:
                         os.remove(self.checkpoint_path)
                 elif model_parameters is not None and self.checkpoint_path is None:
-                    logger.info("Sharding Passed Parameters")
                     if not isinstance(model_parameters, flax.core.FrozenDict):
                         logger.warn(
                             "Model Parameters should be like FrozenDict({'params': params}) make sure to "
