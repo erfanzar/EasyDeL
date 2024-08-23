d5ec064542d00ef38be9a69a1faa7e145052e59b
erfanzar <erfanzare82@yahoo.com>
2024-08-23 16:48:38 +0330

    adding PhiMoe from Phi3.5 building blocks (no test!)

--- src/easydel/modules/phimoe/phimoe_configuration.py
+++ src/easydel/modules/phimoe/phimoe_configuration.py
@@ -1,126 +1,149 @@
 from typing import Optional
 
+from easydel.modules.modeling_utils import EDPretrainedConfig
 from jax.sharding import PartitionSpec
 
-from easydel.modules.modeling_utils import EDPretrainedConfig
 
-
-class Phi3Config(EDPretrainedConfig):
+class PhiMoeConfig(EDPretrainedConfig):
     """
     Configuration objects inherit from [`EDPretrainedConfig`] and can be used to control the model outputs. Read
     the documentation from [`EDPretrainedConfig`] for more information.
 
+
     Args:
         vocab_size (`int`, *optional*, defaults to 32064):
-            Vocabulary size of the Phi-3 model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed to the forward method.
-        hidden_size (`int`, *optional*, defaults to 3072):
-            Dimensionality of the encoder layers and the pooler layer.
-        intermediate_size (`int`, *optional*, defaults to 8192):
-            Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+            Vocabulary size of the PhiMoE model. Defines the number of different tokens that can be represented by the
+            `inputs_ids` passed when calling [`PhiMoEModel`]
+        hidden_size (`int`, *optional*, defaults to 4096):
+            Dimension of the hidden representations.
+        intermediate_size (`int`, *optional*, defaults to 6400):
+            Dimension of the MLP representations.
         num_hidden_layers (`int`, *optional*, defaults to 32):
             Number of hidden layers in the Transformer encoder.
         num_attention_heads (`int`, *optional*, defaults to 32):
             Number of attention heads for each attention layer in the Transformer encoder.
-        num_key_value_heads (`int`, *optional*):
-            Number of key and value heads for each attention layer in the Transformer encoder. Will default to
-            `num_attention_heads` if not set.
-        resid_pdrop (`float`, *optional*, defaults to 0.0):
-            The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
-        embd_pdrop (`float`, *optional*, defaults to 0.0):
-            The dropout ratio for the embeddings.
-        attention_dropout (`float`, *optional*, defaults to 0.0):
-            The dropout ratio for the attention probabilities.
+        num_key_value_heads (`int`, *optional*, defaults to 8):
+            This is the number of key_value heads that should be used to implement Grouped Query Attention. If
+            `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
+            `num_key_value_heads=1 the model will use Multi Query Attention (MQA) otherwise GQA is used. When
+            converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
+            by meanpooling all the original heads within that group. For more details checkout [this
+            paper](https://arxiv.org/pdf/2305.13245.pdf). If it is not specified, will default to `8`.
         hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
-            The non-linear activation function (function or string) to use in the encoder and pooler. If string,
-            `"gelu"`, `"relu"`, `"swish"` and `"gelu_new"` are supported.
-        max_position_embeddings (`int`, *optional*, defaults to 4096):
-            The maximum sequence length that this model might ever be used with. Typically set this to something large
-            just in case (e.g., 2048 or 4096).
-        original_max_position_embeddings (`int`, *optional*, defaults to 4096):
-            The original maximum sequence length that this model might ever be used with. Typically set this to
-            something large just in case (e.g., 2048 or 4096).
+            The non-linear activation function (function or string) in the decoder.
+        max_position_embeddings (`int`, *optional*, defaults to `4096*32`):
+            The maximum sequence length that this model might ever be used with. Mixtral's sliding window attention
+            allows sequence of up to 4096*32 tokens.
         initializer_range (`float`, *optional*, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        rms_norm_eps (`float`, *optional*, defaults to 1e-5):
+        rms_norm_eps (`float`, *optional*, defaults to 1e-05):
             The epsilon used by the rms normalization layers.
         use_cache (`bool`, *optional*, defaults to `True`):
             Whether or not the model should return the last key/values attentions (not used by all models). Only
             relevant if `config.is_decoder=True`.
-        tie_word_embeddings (`bool`, *optional*, defaults to `False`):
-            Whether to tie the weights of the input embeddings and the output embeddings.
-        rope_theta (`float`, *optional*, defaults to 10000.0):
-            The theta value to use for rotary position embeddings.
-        rope_scaling (`Dict[str, Union[str, float]]`, *optional*):
-            The configuration for rope scaling.
+        pad_token_id (`int`, *optional*):
+            The id of the padding token.
         bos_token_id (`int`, *optional*, defaults to 1):
-            The id of the *beginning-of-sequence* token.
-        eos_token_id (`int`, *optional*, defaults to 32000):
-            The id of the *end-of-sequence* token.
-        pad_token_id (`int`, *optional*, defaults to 32000):
-            The index of the padding token in the vocabulary.
+            The id of the "beginning-of-sequence" token.
+        eos_token_id (`int`, *optional*, defaults to 2):
+            The id of the "end-of-sequence" token.
+        tie_word_embeddings (`bool`, *optional*, defaults to `False`):
+            Whether the model's input and output word embeddings should be tied.
+        rope_theta (`float`, *optional*, defaults to 10000.0):
+            The base period of the RoPE embeddings.
+        rope_scaling (`dict`, *optional*):
+            The scaling strategy for the RoPE embeddings. If `None`, no scaling is applied. If a dictionary, it must
+            contain the following keys: `type`, `short_factor`, `long_factor`, `short_mscale`, `long_mscale` and
+            `original_max_position_embeddings`. The `type` must be `longrope`, the `short_mscale` and `long_scale` must
+            be numbers, the `short_factor` and `long_factor` must be lists of numbers with the same length as half of
+            the attention head size and the `original_max_position_embeddings` must be an integer.
         sliding_window (`int`, *optional*):
-            The sliding window size.
+            Sliding window attention window size. If not specified, will default to `262144`.
+        attention_dropout (`float`, *optional*, defaults to 0.0):
+            The dropout ratio for the attention probabilities.
+        num_experts_per_tok (`int`, *optional*, defaults to 2):
+            The number of experts to root per-token, can be also interpreted as the `top-p` routing
+            parameter
+        num_local_experts (`int`, *optional*, defaults to 16):
+            Number of experts per Sparse MLP layer.
+        output_router_logits (`bool`, *optional*, defaults to `False`):
+            Whether or not the router logits should be returned by the model. Enabeling this will also
+            allow the model to output the auxiliary loss. See [here]() for more details
+        router_aux_loss_coef (`float`, *optional*, defaults to 0.0):
+            The aux loss factor for the total loss.
+        router_jitter_noise (`float`, *optional*, defaults to 0.01):
+            Amount of noise to add to the router.
         bits (`int`, *optional*):
             The number of bits to quantize the model to.
         gradient_checkpointing (`str`, *optional*, defaults to `"nothing_saveable"`):
             The gradient checkpointing configuration.
     """
 
-    model_type: str = "phi3"
+    model_type: str = "phimoe"
 
     def __init__(
         self,
         vocab_size=32064,
-        hidden_size=3072,
-        intermediate_size=8192,
+        hidden_size=4096,
+        intermediate_size=6400,
         num_hidden_layers=32,
         num_attention_heads=32,
-        num_key_value_heads=None,
-        resid_pdrop=0.0,
-        embd_pdrop=0.0,
-        attention_dropout=0.0,
+        num_key_value_heads=8,
         hidden_act="silu",
-        max_position_embeddings=4096,
-        original_max_position_embeddings=4096,
+        max_position_embeddings=4096 * 32,
         initializer_range=0.02,
         rms_norm_eps=1e-5,
         use_cache=True,
-        tie_word_embeddings=False,
-        rope_theta=10000.0,
-        rope_scaling=None,
+        pad_token_id=None,
         bos_token_id=1,
-        eos_token_id=32000,
-        pad_token_id=32000,
+        eos_token_id=2,
+        tie_word_embeddings=False,
+        rope_theta=1e6,
+        rope_scaling=None,
         sliding_window=None,
+        attention_dropout=0.0,
+        num_experts_per_tok=2,
+        num_local_experts=16,
+        output_router_logits=False,
+        router_aux_loss_coef=0.001,
+        router_jitter_noise=0.01,
+        input_jitter_noise=0.0,
+        attention_bias=False,
+        lm_head_bias=False,
         bits: Optional[int] = None,
         gradient_checkpointing: str = "nothing_saveable",
         **kwargs,
     ) -> None:
         self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
         self.hidden_size = hidden_size
         self.intermediate_size = intermediate_size
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
-
+        self.sliding_window = sliding_window
+        self.attention_bias = attention_bias
+        self.lm_head_bias = lm_head_bias
+        # for backward compatibility
         if num_key_value_heads is None:
             num_key_value_heads = num_attention_heads
 
         self.num_key_value_heads = num_key_value_heads
-        self.resid_pdrop = resid_pdrop
-        self.embd_pdrop = embd_pdrop
-        self.attention_dropout = attention_dropout
         self.hidden_act = hidden_act
-        self.max_position_embeddings = max_position_embeddings
-        self.original_max_position_embeddings = original_max_position_embeddings
         self.initializer_range = initializer_range
         self.rms_norm_eps = rms_norm_eps
         self.use_cache = use_cache
         self.rope_theta = rope_theta
+        self.attention_dropout = attention_dropout
+
+        self.num_experts_per_tok = num_experts_per_tok
+        self.num_local_experts = num_local_experts
+        self.output_router_logits = output_router_logits
+        self.router_aux_loss_coef = router_aux_loss_coef
+        self.router_jitter_noise = router_jitter_noise
+        self.input_jitter_noise = input_jitter_noise
+
         self.rope_scaling = rope_scaling
         self._rope_scaling_validation()
-        self.sliding_window = sliding_window
-
         self.bits = bits
         self.gradient_checkpointing = gradient_checkpointing
         super().__init__(
@@ -230,21 +253,28 @@ class Phi3Config(EDPretrainedConfig):
         )
 
     def _rope_scaling_validation(self):
-        """Validate the `rope_scaling` configuration."""
+        """
+        Validate the `rope_scaling` configuration.
+        """
         if self.rope_scaling is None:
             return
 
-        if not isinstance(self.rope_scaling, dict) or len(self.rope_scaling) != 3:
+        if not isinstance(self.rope_scaling, dict) or len(self.rope_scaling) != 6:
             raise ValueError(
-                "`rope_scaling` must be a dictionary with three fields, `type`, `short_factor` and `long_factor`, "
-                f"got {self.rope_scaling}"
+                "`rope_scaling` must be a dictionary with three fields, `type`, `short_factor`, `long_factor`, "
+                f"`short_mscale`, `long_mscale` and `original_max_position_embeddings`, got {self.rope_scaling}"
             )
         rope_scaling_type = self.rope_scaling.get("type", None)
         rope_scaling_short_factor = self.rope_scaling.get("short_factor", None)
         rope_scaling_long_factor = self.rope_scaling.get("long_factor", None)
-        if rope_scaling_type is None or rope_scaling_type not in ["su", "yarn"]:
+        rope_scaling_short_mscale = self.rope_scaling.get("short_mscale", None)
+        rope_scaling_long_mscale = self.rope_scaling.get("long_mscale", None)
+        original_max_position_embeddings = self.rope_scaling.get(
+            "original_max_position_embeddings", None
+        )
+        if rope_scaling_type is None or rope_scaling_type not in ["longrope"]:
             raise ValueError(
-                f"`rope_scaling`'s type field must be one of ['su', 'yarn'], got {rope_scaling_type}"
+                f"`rope_scaling`'s type field must be one of ['longrope'], got {rope_scaling_type}"
             )
         if not (
             isinstance(rope_scaling_short_factor, list)
@@ -273,6 +303,18 @@ class Phi3Config(EDPretrainedConfig):
         ):
             raise ValueError(
                 f"`rope_scaling`'s long_factor field must have length {self.hidden_size // self.num_attention_heads // 2}, got {len(rope_scaling_long_factor)}"
+            )
+        if not isinstance(rope_scaling_short_mscale, (int, float)):
+            raise ValueError(
+                f"`rope_scaling`'s short_mscale field must be a number, got {rope_scaling_short_mscale}"
+            )
+        if not isinstance(rope_scaling_long_mscale, (int, float)):
+            raise ValueError(
+                f"`rope_scaling`'s long_mscale field must be a number, got {rope_scaling_long_mscale}"
+            )
+        if not isinstance(original_max_position_embeddings, int):
+            raise ValueError(
+                f"`rope_scaling`'s original_max_position_embeddings field must be an integer, got {original_max_position_embeddings}"
             )
 
     @property
