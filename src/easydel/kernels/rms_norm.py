41eb008582f6aa3ac9674f17cc109bbb2efbf00c
erfanzar <erfanzare82@yahoo.com>
2024-09-02 20:44:25 -0700

    fixing rmsnorm kernel

--- src/easydel/kernels/rms_norm.py
+++ src/easydel/kernels/rms_norm.py
@@ -1,191 +1,198 @@
-import functools
+from functools import partial
 
 import jax.experimental.pallas as pl
 import jax.random
-
-from easydel.kernels.utils import calculate_settings
-from easydel.modules.common import RMSNorm
+from flax import linen as nn
+from jax import lax
 from jax import numpy as jnp
 
 
-# (x * (1 / sqrt(sum(x * x) / cols + eps))) * w
-def _rms_norm_forward_kernel(w_ref, x_ref, o_ref, r_ref, *, eps, dim, exp_tun):
-	org_dtype = x_ref[...].dtype
-	X_R = x_ref[...].astype(jnp.float32)  # make sure it's always in Float32
+class RMSNorm(nn.Module):
+	dim: int
+	eps: float = 1e-6
+	dtype: jnp.dtype = jnp.float32
+	param_dtype: jnp.dtype = jnp.float32
 
-	if exp_tun:
-		row_var = jnp.sum(X_R * X_R, axis=0) / dim
-		inv_var = jax.lax.rsqrt(row_var + eps)
-		r_ref[...] = inv_var
-		normed = X_R * inv_var
-		o_ref[...] = (normed * (w_ref[...] + 1.0)).astype(org_dtype)
-	else:
-		inv_var = jax.lax.rsqrt((jnp.sum(X_R * X_R, -1, keepdims=True) / dim) + eps)
-		r_ref[...] = inv_var
-		normed = (X_R * inv_var).astype(w_ref.dtype)
-		o_ref[...] = (w_ref[...] * normed).astype(org_dtype)
-
-
-def _rms_norm_backward_kernel(
-	g_ref, x_ref, w_ref, r_ref, dx_ref, dw_ref, *, eps, dim, exp_tun
-):
-	normed = x_ref[...] * r_ref[...]
-	dw_ref[...] = (g_ref[...] * normed).astype(dw_ref.dtype)
-	grad_norm = g_ref[...] * w_ref[...]
-	grad_X_R = grad_norm * r_ref[...]
-
-	grad_inv_var = jnp.sum(grad_norm * x_ref[...], axis=-1, keepdims=True)
-
-	grad_X_R_from_inv_var = (
-		-0.5
-		* grad_inv_var
-		* (
-			x_ref[...]
-			/ (jnp.sum(x_ref[...] * x_ref[...], -1, keepdims=True) / dim + eps) ** 1.5
+	def setup(self) -> None:
+		self.weight = self.param(
+			"kernel",
+			nn.initializers.ones,
+			(self.dim,),
+			self.param_dtype,
 		)
+
+	def _norm(self, x: jnp.ndarray) -> jnp.ndarray:
+		return x * lax.rsqrt(jnp.square(x).mean(-1, keepdims=True) + self.eps)
+
+	def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+		x = x.astype(jnp.promote_types(self.dtype, jnp.float32))
+		output = self._norm(x).astype(self.dtype)
+
+		weight = self.weight.astype(self.dtype)
+		return weight * output
+
+
+def _pl_fwd_rms_kernel(x_ref, w_ref, r_ref, o_ref, *, blocksize_x, eps, po_dtype):
+	pid = pl.program_id(0)
+	xslice = (pl.dslice(pid * blocksize_x, blocksize_x), pl.dslice(None))
+	xi = (pid * blocksize_x + jnp.arange(blocksize_x) < x_ref.shape[0])[:, None]
+	xj = (jnp.arange(x_ref.shape[1]) < x_ref.shape[1])[None, :]
+	xmask = xi & xj
+	x = pl.load(x_ref, xslice, mask=xmask).astype(po_dtype)
+	scale = jax.lax.rsqrt(jnp.mean(jnp.square(x), keepdims=True, axis=-1) + eps)
+	pl.store(
+		r_ref,
+		(pl.dslice(pid * blocksize_x, blocksize_x), pl.dslice(None)),
+		scale,
+		mask=xi & jnp.array([True])[None, :],
 	)
+	_normed = scale * x
+	w = pl.load(w_ref, pl.dslice(0, x_ref.shape[1]))
+	o = _normed.astype(o_ref.dtype) * w
+	pl.store(o_ref, xslice, o, mask=xmask)
 
-	dx_ref[...] = grad_X_R + grad_X_R_from_inv_var
 
-
-def _rms_norm_backward_main(
-	eps: float,
-	interpret: bool,
-	exp_tun: bool,
-	res,
-	g,
-):
-	X, R, W = res
-	B, S, DIM = g.shape
-	g = g.reshape(-1, DIM)
-	block_size, num_wraps = calculate_settings(DIM, X.dtype)
-
-	out_shape = [
-		jax.ShapeDtypeStruct(shape=X.shape, dtype=X.dtype, sharding=X.sharding),  # X
-		jax.ShapeDtypeStruct(shape=W.shape, dtype=W.dtype, sharding=W.sharding),  # W
-	]
+def _call_fwd_rms_kernel(x, w, blocksize_x, eps, po_dtype):
+	assert x.ndim == 2 and w.ndim == 1
 	in_specs = [
-		pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
-		pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
-		pl.BlockSpec(
-			lambda i: (0, 0),
-			(
-				1,
-				DIM,
-			),
-		),
-		pl.BlockSpec(lambda i: (0, 0), (S, 1)),
+		pl.BlockSpec(x.shape, lambda *p: (0, 0)),
+		pl.BlockSpec(w.shape, lambda *p: (0,)),
 	]
 	out_specs = [
-		pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),  # X
-		pl.BlockSpec(
-			lambda i: (0, 0),
-			(
-				1,
-				DIM,
-			),
-		),  # W
+		pl.BlockSpec((x.shape[0], 1), lambda *p: (0, 0)),
+		pl.BlockSpec(x.shape, lambda *p: (0, 0)),
 	]
-	method = pl.pallas_call(
-		functools.partial(
-			_rms_norm_backward_kernel,
-			eps=eps,
-			dim=DIM,
-			exp_tun=exp_tun,
-		),
-		out_shape=out_shape,
-		in_specs=in_specs,
-		out_specs=out_specs,  # type:ignore
-		grid=(block_size,),
-		interpret=interpret,
-		name="rms_backward_main",
-		debug=True,
-	)
-
-	# g_ref,
-	# x_ref,
-	# w_ref,
-	# r_ref,
-	# dy_ref,
-
-	dx, dw = method(
-		g,
-		X,
-		W,
-		R,
-	)
-
-	return dx.reshape(B, S, DIM), dw
-
-
-def _rms_norm_forward_main(
-	X: jax.Array, W: jax.Array, eps: float, interpret: bool = False, exp_tun: bool = False
-):
-	B, S, DIM = X.shape
-	W = W.reshape(1, -1)
-	X = X.reshape(-1, DIM)
-	block_size, num_wraps = calculate_settings(DIM, X.dtype)
-
 	out_shape = [
-		jax.ShapeDtypeStruct(shape=X.shape, dtype=X.dtype, sharding=X.sharding),
-		jax.ShapeDtypeStruct(shape=(S, 1), dtype=jnp.float32),
+		jax.ShapeDtypeStruct(shape=(x.shape[0], 1), dtype=po_dtype),
+		jax.ShapeDtypeStruct(shape=x.shape, dtype=x.dtype),
 	]
-	in_specs = [
-		pl.BlockSpec(
-			lambda i: (0, 0),
-			(
-				1,
-				DIM,
-			),
-		),
-		pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
-	]
-	out_specs = [
-		pl.BlockSpec(lambda i: (i, 0), (S * B, DIM)),
-		pl.BlockSpec(lambda i: (0, 0), (S, 1)),
-	]
-	method = pl.pallas_call(
-		functools.partial(
-			_rms_norm_forward_kernel,
+	return pl.pallas_call(
+		f=partial(
+			_pl_fwd_rms_kernel,
+			blocksize_x=blocksize_x,
 			eps=eps,
-			dim=DIM,
-			exp_tun=exp_tun,
+			po_dtype=po_dtype,
 		),
-		out_shape=out_shape,
 		in_specs=in_specs,
-		out_specs=out_specs,  # type:ignore
-		grid=(block_size,),
-		interpret=interpret,
-		name="rms_forward_main",
-		debug=True,
+		out_specs=out_specs,
+		out_shape=out_shape,
+		interpret=True,
+		debug=False,
+		grid=(pl.cdiv(x.shape[0], blocksize_x)),
+	)(x, w)
+
+
+def _call_fwd_rms_kernel_g(x, w, blocksize_x, eps, po_dtype):
+	scale, o = _call_fwd_rms_kernel(
+		x=x,
+		w=w,
+		blocksize_x=blocksize_x,
+		eps=eps,
+		po_dtype=po_dtype,
 	)
-	result, R = method(W, X)
-	return result.reshape(B, S, DIM), (X, R, W)
+
+	return o, (
+		x,
+		w,
+		scale,
+		blocksize_x,
+		eps,
+	)
 
 
-@functools.partial(jax.custom_vjp, nondiff_argnums=[2, 3, 4])
-def rms_norm(
-	X: jax.Array, W: jax.Array, eps: float, interpret: bool = False, exp_tun: bool = False
+def _call_bwd_rms_kernel(po_dtype, res, gin):
+	(
+		x,
+		w,
+		scale,
+		blocksize_x,
+		eps,
+	) = res
+
+
+@partial(jax.custom_vjp, nondiff_argnums=(4,))
+def _rms_call(
+	x,
+	w,
+	blocksize_x,
+	eps,
+	po_dtype,
 ):
-	return _rms_norm_forward_main(
-		W=W, X=X, eps=eps, interpret=interpret, exp_tun=exp_tun
+	return _call_fwd_rms_kernel_g(
+		x=x,
+		w=w,
+		blocksize_x=blocksize_x,
+		eps=eps,
+		po_dtype=po_dtype,
 	)[0]
 
 
-rms_norm.defvjp(_rms_norm_forward_main, _rms_norm_backward_main)
-if __name__ == "__main__":
-	inputs = jax.random.normal(jax.random.key(564), (1, 256, 64), dtype=jnp.float16)
-	print(inputs.devices())
-	norm = RMSNorm(64, 1e-6, jnp.float16, jnp.float16)
-	params = norm.init(jax.random.PRNGKey(0), inputs)
+_rms_call.defvjp(_call_fwd_rms_kernel_g, _call_bwd_rms_kernel)
 
-	out, grad_org = jax.value_and_grad(lambda p: jnp.mean(norm.apply(p, inputs)))(params)
 
-	norm_kernel_out, grad = jax.value_and_grad(
-		lambda w: jnp.mean(rms_norm(X=inputs, W=w, eps=1e-6, interpret=True))
-	)(
-		params["params"]["kernel"],
+def rms_norm(
+	x: jax.Array,
+	w: jax.Array,
+	*,
+	blocksize_x: int = 8,
+	eps: float = 1e-5,
+	po_dtype: jnp.dtype = jnp.float32,
+):
+	return _rms_call(
+		x,
+		w,
+		blocksize_x,
+		eps,
+		po_dtype,
 	)
-	print(jnp.allclose(out, norm_kernel_out))
-	print(grad)
-	print(grad_org["params"]["kernel"])
+
+
+def test_fwd_call():
+	dim = 4
+	seq = 4
+	eps = 1e-5
+	inputs = jax.random.normal(jax.random.key(564), (seq, dim), dtype=jnp.float16)
+
+	norm = RMSNorm(dim, eps, jnp.float16, jnp.float16)
+	params = norm.init(jax.random.PRNGKey(0), inputs)
+	y = norm.apply(params, inputs)
+	y_ = rms_norm(
+		inputs,
+		params["params"]["kernel"],
+		blocksize_x=8,
+		eps=eps,
+		po_dtype=jnp.float32,
+	)
+	print(jnp.allclose(y_, y, atol=0.125, rtol=0))
+	print(y_)
+	# print(y)
+
+
+def test_bwd_call():
+	# FIX BWD
+	dim = 4
+	seq = 4
+	eps = 1e-5
+	inputs = jax.random.normal(jax.random.key(564), (seq, dim), dtype=jnp.float16)
+
+	norm = RMSNorm(dim, eps, jnp.float16, jnp.float16)
+	params = norm.init(jax.random.PRNGKey(0), inputs)
+	g = jax.grad(lambda *x: jnp.sum(norm.apply(*x)))(params, inputs)["params"]["kernel"]
+	g_ = jax.grad(
+		lambda *x: jnp.sum(
+			rms_norm(
+				*x,
+				blocksize_x=8,
+				eps=eps,
+				po_dtype=jnp.float32,
+			)
+		)
+	)(inputs, params["params"]["kernel"])
+	print(jnp.allclose(g, g_, atol=0.125, rtol=0))
+	print(g_)
+	print(g)
+
+
+if __name__ == "__main__":
+	# test_fwd_call()
+	test_bwd_call()
