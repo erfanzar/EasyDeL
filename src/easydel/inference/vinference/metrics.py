3cc83e82622167099c80741c89a0ca54c3c0cff8
erfanzar <erfanzare82@yahoo.com>
2024-10-11 21:36:50 +0330

    improving `vInference` functionality

--- src/easydel/inference/vinference/metrics.py
+++ src/easydel/inference/vinference/metrics.py
@@ -1,3 +1,17 @@
+# Copyright 2023 The EASYDEL Author @erfanzar (Erfan Zare Chavoshi).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import gc
 import threading
 import time
@@ -5,7 +19,11 @@ from dataclasses import dataclass
 
 import jax
 import psutil
-from prometheus_client import Counter, Gauge, Histogram, Info
+
+try:
+	from prometheus_client import Counter, Gauge, Histogram, Info
+except ModuleNotFoundError:
+	Counter, Gauge, Histogram, Info = [None] * 4
 
 
 @dataclass
@@ -172,4 +190,5 @@ class vInferenceMetrics:
 			finally:
 				self.queue_size.labels(model_name=self.model_name).dec()
 				gc.collect()
+
 		return wrapper
