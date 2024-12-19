822763bafaeaf9fe9804833cc1c9e6e503afc86b
erfanzar <erfanzare82@yahoo.com>
2024-12-19 21:15:57 +0330

    Updating `SFTTrianer`

--- tests/trainers/supervised_finetuning_test.py
+++ tests/trainers/supervised_finetuning_test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import flax
+
 dirname = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dirname)
 sys.path.append(
@@ -9,12 +11,7 @@ sys.path.append(
 		"../..",
 	)
 )
-os.environ["EKERNEL_OPS"] = "false"
-# import jax
 
-# jax.config.update("jax_platform_name", "cpu")  # CPU Test !
-
-import flax.core
 from datasets import load_dataset
 from jax import numpy as jnp
 from transformers import AutoTokenizer
@@ -22,55 +19,59 @@ from transformers import AutoTokenizer
 import easydel as ed
 from easydel.trainers import create_prompt_creator
 
+NUM_TRAIN_EPOCHS = 4
+TOTAL_BATCH_SIZE = 8
+
 
 def main():
 	sequence_length = 128
-	config = ed.MistralConfig(
-		hidden_size=128,
+	config = ed.LlamaConfig(
+		head_dim=16,
+		hidden_size=64,
 		num_attention_heads=8,
 		num_key_value_heads=4,
-		num_hidden_layers=4,
-		intermediate_size=256,
-		gradient_checkpointing=ed.EasyDeLGradientCheckPointers.NOTHING_SAVEABLE,
+		num_hidden_layers=1,
+		intermediate_size=128,
 		max_position_embeddings=sequence_length,
-		attn_dtype=jnp.float16,
+		attn_dtype=jnp.float32,
 		attn_mechanism=ed.AttentionMechanisms.VANILLA,
-		block_k=512,
-		block_q=512,
-		hardware_abstraction=False,
 	)
 
 	tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
 	train_dataset = load_dataset("LDJnr/Pure-Dove", split="train[:5%]")
-
-	model = ed.MistralForCausalLM(config=config, _do_init=True)
-	params = model.shard_model(model.params)
+	dtype = jnp.float32
+	model = ed.LlamaForCausalLM(
+		config=config,
+		dtype=dtype,
+		param_dtype=dtype,
+		rngs=flax.nnx.Rngs(0),
+	)
+	model = model.shard_model()
 
 	prompter = create_prompt_creator(tokenizer)
-	dtype = jnp.float32
 	trainer = ed.SFTTrainer(
 		arguments=ed.TrainingArguments(
-			model_name="SFTTrainer_TEST",
-			num_train_epochs=3,
-			total_batch_size=2,
-			optimizer=ed.EasyDeLOptimizers.ADAMW,
-			scheduler=ed.EasyDeLSchedulers.COSINE,
+			save_directory="tmp-files",
+			model_name="SFT-TrainerTest",
+			num_train_epochs=NUM_TRAIN_EPOCHS,
+			total_batch_size=TOTAL_BATCH_SIZE,
 			gradient_accumulation_steps=2,
-			use_wandb=False,
 			do_train=True,
 			do_eval=False,
 			max_sequence_length=sequence_length,
-			dtype=dtype,
-			param_dtype=dtype,
-			track_memory=False,
-			learning_rate=5e-4,
-			label_smoothing_factor=0.1,
-			z_loss=0.0001,
-			train_on_inputs=True,
-			# save_steps=500,
-			save_total_limit=1,
+			track_memory=True,
+			use_wandb=False,
+			learning_rate=3e-4,
 			do_last_save=True,
+			save_steps=10,
+			save_total_limit=5,
+			save_optimizer_state=True,
+			training_time="80Min",
+			optimizer=ed.EasyDeLOptimizers.ADAMW,
+			scheduler=ed.EasyDeLSchedulers.COSINE,
+			clip_grad=1.0,
+			warmup_steps=5,
 		),
 		model=model,
 		train_dataset=train_dataset,
@@ -78,10 +79,8 @@ def main():
 		formatting_func=prompter,
 		packing=True,
 		num_of_sequences=sequence_length,
-		# chars_per_token=2.1,
-		dataset_num_proc=32,
 	)
-	return trainer.train(model_parameters=flax.core.FrozenDict({"params": params}))
+	return trainer.train()
 
 
 if __name__ == "__main__":
