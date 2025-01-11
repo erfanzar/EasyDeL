28148915f95e1f38c22439157bc7cd2c7c3d7397
erfanzar <erfanzare82@yahoo.com>
2025-01-12 01:56:53 +0330

    Updating `TPU Flags`

--- easydel/__init__.py
+++ easydel/__init__.py
@@ -50,7 +50,27 @@ if _os.environ.get("EASYDEL_AUTO", "true") in ["true", "1", "on", "yes"]:
 	)
 	_os.environ["LIBTPU_INIT_ARGS"] = (
 		_os.environ.get("LIBTPU_INIT_ARGS", "") + " "
-		"--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000  --xla_tpu_enable_latency_hiding_scheduler=true --xla_enable_async_all_gather=true TPU_MEGACORE=MEGACORE_DENSE"
+		"--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 "
+		"--xla_tpu_spmd_threshold_for_allgather_cse=10000  "
+		"--xla_tpu_enable_latency_hiding_scheduler=true "
+		"--xla_tpu_megacore_fusion_allow_ags=false "
+		"--xla_enable_async_collective_permute=true "
+		"--xla_tpu_enable_ag_backward_pipelining=true "
+		"--xla_tpu_enable_data_parallel_all_reduce_opt=true "
+		"--xla_tpu_data_parallel_opt_different_sized_ops=true "
+		"--xla_tpu_enable_async_collective_fusion=true "
+		"--xla_tpu_enable_async_collective_fusion_multiple_steps=true "
+		"--xla_tpu_overlap_compute_collective_tc=true "
+		"--xla_enable_async_all_gather=true " 
+		"--xla_tpu_enable_async_collective_fusion_fuse_all_gather=true " 
+		"TPU_MEGACORE=MEGACORE_DENSE "
+	)
+	_os.environ.update(
+		{
+			"NCCL_LL128_BUFFSIZE": "-2",
+			"NCCL_LL_BUFFSIZE": "-2",
+			"NCCL_PROTO": "SIMPLE,LL,LL128",
+		}
 	)
 	_os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
 	if _os.environ.get("XLA_PYTHON_CLIENT_MEM_FRACTION", None) is None:
