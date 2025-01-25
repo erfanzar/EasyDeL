79c1cc2f802f3e11332b72938c0d93d968954f5f
erfanzar <erfanzare82@yahoo.com>
2025-01-25 23:21:55 +0000

    TODO: improving token generation for vinference

--- tests/vinference_runtime_test.py
+++ tests/vinference_runtime_test.py
@@ -20,10 +20,10 @@ def main():
 	max_length = 2048
 
 	# if jax.default_backend() == "gpu":
-	pretrained_model_name_or_path = "erfanzar/Xerxes2-1B"
+	pretrained_model_name_or_path = "meta-llama/Llama-3.2-1B-Instruct"
 	# else:
 	# 	pretrained_model_name_or_path = "Qwen/Qwen2.5-7B-Instruct"
-
+	# "Qwen/Qwen2-7B-Instruct"
 	extra = {}
 	if jax.default_backend() == "gpu":
 		import torch
@@ -51,7 +51,7 @@ def main():
 		)
 	print(dtype, param_dtype)
 	partition_axis = ed.PartitionAxis()
-	tokenizer = transformers.AutoTokenizer.from_pretrained("Qwen/Qwen2-7B-Instruct")
+	tokenizer = transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
 	tokenizer.padding_side = "left"
 	tokenizer.pad_token_id = tokenizer.eos_token_id
 	print("TOKENIZER LOADED")
@@ -86,7 +86,7 @@ def main():
 			do_sample=False,
 			top_p=0.95,
 			top_k=10,
-			eos_token_id=0,
+			eos_token_id=model.generation_config.eos_token_id,
 			streaming_chunks=32,
 		),
 	)
@@ -125,31 +125,47 @@ def main():
 			padding="max_length",
 			add_generation_prompt=True,
 		)
+		pad_seq = inference.model_prefill_length
+
+		print("Start Generation Process.")
+		for response in inference.generate(**ids):
+			next_slice = slice(
+				pad_seq,
+				pad_seq + inference.generation_config.streaming_chunks,
+			)
+			pad_seq += inference.generation_config.streaming_chunks
+			print(
+				tokenizer.decode(response.sequences[0][next_slice], skip_special_tokens=True),
+				end="",
+			)
+
+		print()
+		print(response.generated_tokens)
+		print("TPS :", response.tokens_pre_second)
+
 	else:
 		ids = tokenizer(
-			"hello how are you?",
+			["hello how are you?"] * 1,
 			return_tensors="jax",
 			max_length=inference.model_prefill_length,
 			padding="max_length",
 			return_attention_mask=True,
 		)
-	pad_seq = inference.model_prefill_length
 
-	print("Start Generation Process.")
-	for response in inference.generate(**ids):
-		next_slice = slice(
-			pad_seq,
-			pad_seq + inference.generation_config.streaming_chunks,
-		)
-		pad_seq += inference.generation_config.streaming_chunks
-		print(
-			tokenizer.decode(response.sequences[0][next_slice], skip_special_tokens=True),
-			end="",
-		)
+		pad_seq = inference.model_prefill_length
 
-	print()
-	print(response.generated_tokens)
-	print("TPS :", response.tokens_pre_second)
+		print("Start Generation Process.")
+		response = None
+		for response in inference.generate(**ids):
+			...
+
+		for i in range(len(response.sequences)):
+			print(f"Batch index {i}")
+			print(tokenizer.decode(response.sequences[i], skip_special_tokens=True))
+			print("-" * 15)
+		print()
+		print(response.generated_tokens)
+		print("TPS :", response.tokens_pre_second)
 
 
 if __name__ == "__main__":
