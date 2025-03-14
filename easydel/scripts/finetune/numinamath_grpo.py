5578037f4f1e5a92cf1bddfe7b92850d351ed17a
erfanzar <erfanzare82@yahoo.com>
2025-03-14 18:55:31 +0000

    fixing print issue

--- easydel/scripts/finetune/numinamath_grpo.py
+++ easydel/scripts/finetune/numinamath_grpo.py
@@ -165,8 +165,7 @@ def main():
 	def format_reward(completions, **kwargs):
 		"""Reward function that checks if the completion has a specific format."""
 		pattern = r"^<think>.*?</think>\s*<answer>.*?</answer>$"
-		completion_contents = [completion[0]["content"] for completion in completions]
-		print(completion_contents)
+		completion_contents = [completion[0]["content"] for completion in completions] 
 		matches = [re.match(pattern, content) for content in completion_contents]
 		rewards_list = [1.0 if match else 0.0 for match in matches]
 		return rewards_list
