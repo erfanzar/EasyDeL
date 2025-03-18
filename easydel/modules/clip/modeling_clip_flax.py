Fixed potential bugs in the Llava model; Updated Splash Attention to fall back to vanilla attention when causal=False

easydel/modules/clip/modeling_clip_flax.py
@@ -627,7 +627,8 @@ class CLIPVisionTransformer(EasyDeLBaseModule):
 		return_dict = (
 			return_dict if return_dict is not None else self.config.use_return_dict
 		)
-
+		if pixel_values is not None and pixel_values.ndim == 4:
+			pixel_values = jnp.swapaxes(pixel_values, 1, 3)
 		hidden_states = self.embeddings(pixel_values)
 		hidden_states = self.pre_layrnorm(hidden_states)
 
@@ -765,8 +766,13 @@ class CLIPTextModelWithProjection(EasyDeLBaseModule):
 
 @register_module(
 	config=CLIPVisionConfig,
-	model_type="clip",
+	model_type="clip_vision_model",
 	task_type=TaskType.BASE_VISION,
+)
+@register_module(
+	config=CLIPVisionConfig,
+	model_type="clip_vision_model",
+	task_type=TaskType.BASE_MODULE,
 )
 class CLIPVisionModel(EasyDeLBaseModule):
 	def __init__(

easydel/modules/llava/modeling_llava_flax.py
@@ -290,7 +290,7 @@ class LlavaForConditionalGeneration(EasyDeLBaseModule):
 		required_props: tp.Optional[tp.Mapping[str, tp.Dict[str, tp.Any]]] = None,
 		**kwargs,
 	):
-		basics = super()._get_compile_model_kwargs(
+		basics = self.language_model._get_compile_model_kwargs(
 			batch_size=batch_size,
 			input_tokens_length=input_tokens_length,
 			input_sharding=input_sharding,
@@ -303,7 +303,6 @@ class LlavaForConditionalGeneration(EasyDeLBaseModule):
 			required_props=required_props,
 			**kwargs,
 		)
-
 		if vision_included:
 			pixel_values = jnp.ones(
 				(
@@ -333,6 +332,8 @@ class LlavaForConditionalGeneration(EasyDeLBaseModule):
 		return model_inputs
 
 	def update_inputs_for_generation(self, model_outputs, model_kwargs):
-		model_kwargs = super().update_inputs_for_generation(model_outputs, model_kwargs)
+		model_kwargs = self.language_model.update_inputs_for_generation(
+			model_outputs, model_kwargs
+		)
 		model_kwargs.pop("pixel_values", None)  # only effect first iter
 		return model_kwargs