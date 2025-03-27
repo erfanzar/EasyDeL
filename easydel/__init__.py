2101c1449d920351137c3df2c0f7e2b2d17d9b1f
erfanzar <erfanzare82@yahoo.com>
2025-03-27 20:31:22 +0330

    Bump version to 0.1.2; update targeted versions; refactor auto_pytree for cleaner dataclass handling; supporting jax version 0.5.3.

--- easydel/__init__.py
+++ easydel/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 import os as _os
 import sys as _sys
@@ -761,7 +761,7 @@ else:
 		extra_objects={"__version__": __version__},
 	)
 
-	_targeted_versions = ["0.0.18.1"]
+	_targeted_versions = ["0.0.18.2"]
 
 	from eformer import __version__ as _eform_version
 
