e3c272d733daa91718077b44c287086c514d2899
erfanzar <erfanzare82@yahoo.com>
2025-03-28 21:39:34 +0330

    Bump eformer version to 0.0.19 for compatibility updates

--- easydel/__init__.py
+++ easydel/__init__.py
@@ -761,7 +761,7 @@ else:
 		extra_objects={"__version__": __version__},
 	)
 
-	_targeted_versions = ["0.0.18.2"]
+	_targeted_versions = ["0.0.19"]
 
 	from eformer import __version__ as _eform_version
 
