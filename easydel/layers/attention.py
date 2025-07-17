fixing attention mask generation issue for sliding mask

easydel/layers/attention.py
@@ -668,7 +668,12 @@ class AttentionModule(nn.Module):
                             jax.lax.dynamic_slice_in_dim(ival, start_index, sliding_window, 0),
                         )
                 elif cache_view is not None and masking_details is not None and mode == common_types.MODE_PREFILL:
-                    imsk = imsk[:, :, query_length - sliding_window : query_length]
+                    imsk = jax.lax.dynamic_slice_in_dim(
+                        imsk,
+                        start_index=max(0, query_length - sliding_window),
+                        slice_size=min(sliding_window, imsk.shape[-1]),
+                        axis=2,
+                    )
                 return ikey, ival, imsk
 
             key, value, attention_mask = _select_slices(key, value, attention_mask, offsets, indexs)