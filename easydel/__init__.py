920e76a8c7190f5742a163e7e2ec144f4d2a948d
erfanzar <erfanzare82@yahoo.com>
2025-08-29 14:19:34 +0000

    chore: bump eformer version to 0.0.52 and update dependency source
      - Update targeted eformer version from 0.0.51 to 0.0.52
      - Switch eformer dependency to git source in pyproject.toml

--- easydel/__init__.py
+++ easydel/__init__.py
@@ -753,7 +753,7 @@ else:
         extra_objects={"__version__": __version__},
     )
 
-    _targeted_versions = ["0.0.51"]
+    _targeted_versions = ["0.0.52"]
 
     from eformer import __version__ as _eform_version
 
